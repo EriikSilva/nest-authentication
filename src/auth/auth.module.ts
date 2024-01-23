2a849d39c6518b203319ebf461769710984ef9e9
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2024-01-23 14:34:11 -0400

    feat:auth implement login validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
@@ -6,7 +6,8 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
-
+import { NestModule } from '@nestjs/common'
+import { LoginValidationMiddleware } from './middleware/login-validation.middleware';
 @Module({
   imports: [PassportModule, UserModule, JwtModule.register({
     secret: process.env.JWT_KEY,
@@ -15,4 +16,8 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy]
 })
-export class AuthModule {}
+export class AuthModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+     consumer.apply(LoginValidationMiddleware).forRoutes('login') 
+  }
+}
