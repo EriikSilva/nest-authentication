765701bd70569b5846c3c210c1fa9f9ff2c809de
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2024-01-23 13:13:33 -0400

    feat:implement local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, UserModule],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt'
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+
+  constructor(private readonly userService:UserService){}
+
+  async validateUser(email: string, senha: string) {
+    const user = await this.userService.findByEmail(email)
+
+    if(user){
+      const isPasswordValid = await bcrypt.compare(senha, user.senha);
+
+      if(isPasswordValid){
+        return {
+          ...user,
+          senha:undefined
+        }
+      }
+
+      throw new Error('Email ou Senha incorretas');
+    }
   }
   
 }
