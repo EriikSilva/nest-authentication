90190965c46c4630b49d8bae31cfa420d729f61e
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2024-01-23 13:55:33 -0400

    feat:login function

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,13 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PassportModule, UserModule],
+  imports: [PassportModule, UserModule, JwtModule.register({
+    secret: process.env.JWT_KEY,
+    signOptions: {expiresIn: '24h'}
+  })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  constructor(private readonly userService:UserService){}
+  login(user: User):UserToken {
+    const { id_usuario, email } = user;
+
+    const payload: UserPayload = {
+      sub: id_usuario,
+      email,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token:jwtToken
+    }
+
+  }
 
   async validateUser(email: string, senha: string) {
-    const user = await this.userService.findByEmail(email)
+    const user = await this.userService.findByEmail(email);
 
-    if(user){
+    if (user) {
       const isPasswordValid = await bcrypt.compare(senha, user.senha);
 
-      if(isPasswordValid){
+      if (isPasswordValid) {
         return {
           ...user,
-          senha:undefined
-        }
+          senha: undefined,
+        };
       }
 
       throw new Error('Email ou Senha incorretas');
     }
   }
-  
 }
