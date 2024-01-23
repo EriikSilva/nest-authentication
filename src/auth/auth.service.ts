4f7f6eb4f35e6d25bdb8038630a1272b9222c468
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2024-01-23 12:55:56 -0400

    pre-request guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-    validateUser(email:string, senha:string){
-        throw new Error("Method Not IMplemented")
-    }
+  validateUser(email: string, password: string) {
+    throw new Error('Method not implemented.');
+  }
+  
 }
