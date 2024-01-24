ef71ea167c5eac3e2a14fcbb07c5583f69186cc1
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2024-01-24 09:26:52 -0400

    prettier

--- prisma/seeder/seeder.ts
+++ prisma/seeder/seeder.ts
@@ -1,19 +1,26 @@
 // prisma/seeder.ts
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function seed() {
   const userData = [
-    { email: 'john.doe@example.com', senha:'Abc@123' },
-    { email: 'jane.doe@example.com', senha:'123@Abc' },
+    { email: 'john.doe@example.com', senha: 'Abc@123' },
+    { email: 'jane.doe@example.com', senha: '123@Abc' },
   ];
 
+  const saltRounds = 10;
+
   for (const user of userData) {
-    await prisma.usuario.create({
-      data: user,
-    });
+    const data = {
+      ...user,
+      senha: await bcrypt.hash(user.senha, saltRounds),
+    };
+
+    await prisma.usuario.create({ data });
   }
+  
 }
 
 seed()
